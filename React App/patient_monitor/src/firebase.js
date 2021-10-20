12efdf5098b18372fc586874f1e46182762992a3
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2021-10-21 01:01:36 +0530

    Update firebase.js

--- React App/patient_monitor/src/firebase.js
+++ React App/patient_monitor/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAQG3ylEix8aFDhedFSJrm10GVuBuY5E5o",
-  authDomain: "iot-covid-patient-monitoring.firebaseapp.com",
-  databaseURL: "https://iot-covid-patient-monitoring-default-rtdb.firebaseio.com",
-  projectId: "iot-covid-patient-monitoring",
-  storageBucket: "iot-covid-patient-monitoring.appspot.com",
-  messagingSenderId: "598066107511",
-  appId: "1:598066107511:web:cbfc683baf896956bca272"
+  apiKey: "Your API Key",
+  authDomain: "Your Firebase auth domain",
+  databaseURL: "Your Firebase Database URL",
+  projectId: "Your Firebase project ID",
+  storageBucket: "Your firebase storage bucket to use with firestore",
+  messagingSenderId: "To enable messaging services like chatbots and social networking chatting applications",
+  appId: "Unique AppID required to identify which app is accessing your firebase"
 })
 
 export const auth = app.auth()
