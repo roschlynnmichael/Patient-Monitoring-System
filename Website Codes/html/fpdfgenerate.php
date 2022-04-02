a194d8536c89da6056f123edad51a37f737fc553
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2022-04-02 22:54:30 +0530

    Update fpdfgenerate.php

--- Website Codes/html/fpdfgenerate.php
+++ Website Codes/html/fpdfgenerate.php
@@ -10,10 +10,10 @@ if(!isset($_SESSION['loggedin']))
 }
 
 $patientid = $_POST['patient_id'];
-$DATABASE_HOST='localhost';
-$DATABASE_USER='admin';
-$DATABASE_PASS='dsouza';
-$DATABASE_NAME='patient_monitoring';
+$DATABASE_HOST='server_ip_or_localhost';
+$DATABASE_USER='username';
+$DATABASE_PASS='password';
+$DATABASE_NAME='db_name';
 
 $con = mysqli_connect($DATABASE_HOST , $DATABASE_USER , $DATABASE_PASS , $DATABASE_NAME);
 
