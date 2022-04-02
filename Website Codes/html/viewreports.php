bbdf079bd16414b74a8c5439aadeaff98f980a00
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2022-04-02 23:07:47 +0530

    Update viewreports.php

--- Website Codes/html/viewreports.php
+++ Website Codes/html/viewreports.php
@@ -87,10 +87,10 @@ if(!isset($_SESSION['loggedin']))
                             <select name="view_data" id="view_data" onchange="view_data()" class="select form-control-lg">
                                 <option>Select Patient to view entire report</option>
                                     <?php
-                                        $DATABASE_HOST='localhost';
-                                        $DATABASE_USER='admin';
-                                        $DATABASE_PASS='dsouza';
-                                        $DATABASE_NAME='patient_monitoring';
+                                        $DATABASE_HOST='server_ip_or_localhost';
+                                        $DATABASE_USER='username';
+                                        $DATABASE_PASS='password';
+                                        $DATABASE_NAME='db_name';
                                             
                                         $con = mysqli_connect($DATABASE_HOST , $DATABASE_USER , $DATABASE_PASS , $DATABASE_NAME);
                                             
